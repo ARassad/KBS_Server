c93fa4c48ea00e02c19ce296b78fa5582e89d5c8
Arkadi <arassadnikov97@gmail.com>
2018-04-10 11:37:51 +0300

    Исправление создания проверки, теперь устанавливает созданную проверку  предприятию как текущую

--- CreateCheck.py
+++ CreateCheck.py
@@ -8,10 +8,16 @@ class CreateCheck(Request):
 
         companyId, deputyId, doctorId, policeId, doomId = params["companyId"], params["deputyId"], params["doctorId"], params["policeId"], params["doomId"]
 
-        cursor.execute("INSERT INTO Examination(id_company, id_deputy, id_duma_specialist, id_doctor, id_policeman, status) VALUES ({}, {}, {}, {}, {}, '{}')"
-                                        .format(companyId, deputyId, doomId, doctorId, policeId, "inProgres"))
+        cursor.execute("INSERT INTO Examination(id_company, id_deputy, id_duma_specialist, id_doctor, id_policeman, status) OUTPUT INSERTED.id VALUES ({}, {}, {}, {}, {}, '{}')"
+                        .format(companyId, deputyId, doomId, doctorId, policeId, "inProgres"))
 
-        dataTransferObject.resultRequest = "True"
+        row = cursor.fetchone()
+
+        if row is not None:
+            last_exam_id = row[0]
+
+            cursor.execute("UPDATE Company_User SET id_current_exam = {} WHERE id = {}".format(last_exam_id, companyId))
+
+            dataTransferObject.resultRequest = "True"
 
         return
-
