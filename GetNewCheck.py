9cc9430f889859783af9cb46c9a8f7dae7b633bb
Arkadi <arassadnikov97@gmail.com>
2018-04-15 12:29:36 +0300

    Поправлена команда получения оценки специалиста

--- GetNewCheck.py
+++ GetNewCheck.py
@@ -12,10 +12,22 @@ class GetNewCheck(Request):
 
         cursor.execute("SELECT id, [name] FROM Company_User WHERE id_current_exam IN (SELECT id FROM Examination WHERE id_duma_specialist = {id} OR id_doctor = {id} OR id_policeman = {id})"
                        .format(id=specId))
-        row = cursor.fetchone()
+        company = cursor.fetchone()
 
-        if row is not None:
+        if company is not None:
+            cursor.execute("SELECT type_specialist FROM Specialist_User WHERE id = {}".format(specId))
+            type = cursor.fetchone()[0]
+
+            types = ["id_doctor", "id_policeman", "id_duma_specialist"]
+            type_crit = ["goods", "security", "sanitation"]
+
+            cursor.execute("SELECT {} FROM Examination WHERE {} = {} AND id_company = {} ".format(type_crit[type], types[type], specId, company[0]))
+            row = cursor.fetchone()
+            if row is not None and row[0] is not None:
+                return
+
+        if company is not None:
             dataTransferObject.isCheckExist = "True"
 
-            dataTransferObject.companyId = row[0]
-            dataTransferObject.companyName = row[1]
+            dataTransferObject.companyId = company[0]
+            dataTransferObject.companyName = company[1]
