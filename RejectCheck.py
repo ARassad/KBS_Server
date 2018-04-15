e222e014f2a337ed2253cf00f64f0cb90e0478a3
Arkadi <arassadnikov97@gmail.com>
2018-04-15 23:37:25 +0300

    hotfix

--- RejectCheck.py
+++ RejectCheck.py
@@ -8,13 +8,13 @@ class RejectCheck(Request):
 
         companyId = int(params["companyId"])
 
-        cursor.execute("SELECT id_last_exam FROM Company_User WHERE id = {}".format(companyId))
+        cursor.execute("SELECT id_current_exam FROM Company_User WHERE id = {}".format(companyId))
         row = cursor.fetchone()
 
         if row is not None and row[0] is not None:
             id_current_exam = row[0]
 
-            cursor.execute("UPDATE Examination SET goods=null, sanitation=null, security=null, consumerRights=null, status ='{}' WHERE id = {}".format("inProgres", id_current_exam))
+            cursor.execute("UPDATE Examination SET goods=null, sanitation=null, security=null, consumerRights=null WHERE id = {} AND status ='inProgres'".format(id_current_exam))
 
             #cursor.execute("UPDATE Company_User SET id_current_exam={}, id_last_exam=null WHERE id = {}".format(id_current_exam, companyId))
 
